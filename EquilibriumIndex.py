09eadbec7d24e0e3fa90caf6132049edcfaca8ae
nsimoneaux <nsimoneaux@gmail.com>
2016-06-02 18:01:08 -0700

    adding all

--- EquilibriumIndex.py
+++ EquilibriumIndex.py
@@ -1,2 +1,25 @@
+#I gathered my solution from the following web page: https://rosettacode.org/wiki/Equilibrium_index
+
+#Import the default dictonary subclass for missing values
+from collections import defaultdict
+
+
+#define method and approriate argument
 def eqindex(data):
-    #do code here
+    
+    #instantiate l and h=0, and instantiate the default dictionary values in the to '0'
+    l, h = 0, defaultdict(list)
+    
+    #for each index 'i' and value 'c' in list 'data'...
+    for i, c in enumerate(data): 
+        
+        #add the value 'c' to l (the total sum of the list so far)
+        l += c
+        
+        #append the index 'i' to the array 'h', at the index of array 'h' where double the value of 'l' subtracting c
+        #for values of l and c where c is half of l, this will accumlate all positions of i
+        h[l * 2 - c].append(i)
+        
+    #return the value of array 'h' at the index 'l', calculated in the previous step to have accumulated all positions ('i') of the list where 'l', doubled, subtracting 'c' are equal to 'l' (equilibrium cases) 
+    return h[l]
+    
--- TrueBooj.py
+++ TrueBooj.py
@@ -1,2 +1,18 @@
 def truebooj(number):
-    # do code here
+    #instantiate an array of attributes to be applied to a given number
+    attributes = []
+    
+    #if a number cleanly divides by three, add "true" to the attributes array
+    if (number % 3 == 0):
+        attributes.append("True")
+        
+    #if a number cleanly divides by five, add "booj" to the attributes array    
+    if (number % 5 == 0):
+        attributes.append("Booj")
+        
+    #if a number cleanly divides by ten, add "true booj" to the attributes array    
+    if (number % 10 == 0):
+        attributes.append("True Booj")
+        
+    #the if statements are constructed to be non-mutually exclusive -- ex: 30 should have all three attributes 
+    print number, attributes
